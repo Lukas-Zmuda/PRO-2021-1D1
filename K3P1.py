a7ccd8b50ce319183634b0ff0f03ac6fd6496cc4
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-11 12:33:46 +0200

    Update K3P1.py

--- K3P1.py
+++ K3P1.py
@@ -1,9 +1,4 @@
 import tkinter
-canvas = tkinter.Canvas()
+canvas=tkinter.Canvas()
 
-canvas.create_line(20, 50, 100, 50, fill = 'blue',width = 3)
-canvas.create_line(20, 50, 20, 100, width = 5)
-canvas.create_line(20, 100, 100, 50, fill = 'red')
-
-canvas.create_line(20, 100, 100, 100, 20, 150, 20, 100, fill = 'green', width = 2)
 canvas.pack()
