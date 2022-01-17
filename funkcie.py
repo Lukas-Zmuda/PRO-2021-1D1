58fa1111d453e929bd6e450ccc7c1304afd88a81
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-01-17 10:20:22 +0100

    funkcie

--- funkcie.py
+++ funkcie.py
@@ -9,19 +9,22 @@ def skola():
     print("| Stredna odborna skola informacnych technologii |")
     print("+------------------------------------------------+")
 
-
-skola()
-print("Fero")
-print("daco")
-skola()
-print("********************")
-#for i in range(5):
-#    skola()
+#volanie funkcie
+#skola()
+#print("Fero")
+#print("daco")
+#skola()
+#print("********************")
+for i in range(5):
+    skola()
 
 def vitaj(meno_studenta):
     skola()
     print("Vitaj " + meno_studenta + " na nasej skole.")
 
+def privitanie(meno_studenta, rok_narodenia):
+    vek = 2021 - rok_narodenia
+    print("Ahoj " + meno_studenta + ". Mas " + str(vek) + " rokov.")
 vitaj("Peter")
 
 vitaj("Katka")
