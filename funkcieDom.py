63d32f287796ec8c7e4535d17a8cfd5a0f6c51a2
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-02-08 09:33:34 +0100

    Update funkcieDom.py

--- funkcieDom.py
+++ funkcieDom.py
@@ -1,14 +1,22 @@
 import tkinter
-from random import *
-
-canvas = tkinter.Canvas()
+canvas = tkinter.Canvas(height = 800, width = 800)
 canvas.pack()
 
-def dom():
-    x = randint(100, 300)
-    y = randint(100, 300)
-    canvas.create_rectangle(x, y, x+50, y+50)
-    canvas.create_line(x, y, x+25, y-50, x+50, y)
-dom()
-dom()
-dom()
+canvas.create_rectangle (100,100,350,500, fill = "yellow", outline = "yellow")
+canvas.create_oval (100,50,350,150, fill = "brown", outline = "brown")
+canvas.create_oval (75,100,375,150, fill = "brown", outline = "brown")
+
+canvas.create_line(220,225,230,225, fill = "black",width = 4)
+canvas.create_oval (230,190,320,260, fill = "yellow", outline = "black",width = 4)
+canvas.create_oval (130,190,220,260, fill = "yellow", outline = "black",width = 4)
+canvas.create_line(90,175,130,225, fill = "black",width = 4)
+canvas.create_line(360,175,320,225, fill = "black",width = 4)
+
+
+canvas.create_oval (150,200,200,250, fill = "blue", outline = "blue")
+canvas.create_oval (250,200,300,250, fill = "blue", outline = "blue")
+
+canvas.create_rectangle (220, 250, 230, 350, fill = "blue", outline = "blue")
+
+canvas.create_oval (130, 360, 320, 435, fill = "red", outline = "red")
+canvas.create_oval (130, 380, 320, 450, fill = "yellow", outline = "yellow")
